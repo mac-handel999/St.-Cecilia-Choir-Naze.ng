7f408358906a5e4774a0824908965871bc787c07
mac-handel999 <fabikechukwuemeana@gmail.com>
2024-07-26 12:53:36 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+    const p = document.querySelector('.acctno');
+        
+    const donateBtn  = document.querySelector('.donate');
+
+  donateBtn.addEventListener('click', () => {
+    
+    if (p.innerHtml === " ") {
+      p.innerHtml ="St.Cecilia Choir, 1234567891011,G.T Bank";
+    } else {
+      p.innerHtml = " ";
+    }
+
+   });
+  
